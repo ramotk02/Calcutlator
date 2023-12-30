f0ca95fbfa337a845c1f68dfc8beb2c8c8dbbf51
ramotk02 <omartaky.ot@gmail.com>
2023-12-30 18:45:58 +0100

    up

---
+++ code.js
@@ -0,0 +1,4 @@
+let  = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.querySelector("form");
