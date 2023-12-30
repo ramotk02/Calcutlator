31c4d7fe3ff51e14d0b6f9e62096cb1707d53caf
ramotk02 <omartaky.ot@gmail.com>
2023-12-30 19:13:00 +0100

    js

--- code.js
+++ code.js
@@ -1,4 +1,36 @@
-let  = document.getElementById("name");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let form = document.querySelector("form");
+let displayValue = '0';
+
+      function appendToDisplay(value) {
+        if (displayValue === '0' && value !== '.') {
+          displayValue = value;
+        } else {
+          displayValue += value;
+        }
+        updateDisplay();
+      }
+
+      function deleteFromDisplay() {
+        displayValue = displayValue.slice(0, -1);
+        if (displayValue === '') {
+          displayValue = '0';
+        }
+        updateDisplay();
+      }
+
+      function resetDisplay() {
+        displayValue = '0';
+        updateDisplay();
+      }
+
+      function calculate() {
+        try {
+          displayValue = eval(displayValue).toString();
+        } catch (error) {
+          displayValue = 'Error';
+        }
+        updateDisplay();
+      }
+
+      function updateDisplay() {
+        document.getElementById('display').innerText = displayValue;
+      }
