ba0d39ce66d91932b9278a07d6e31cab58abbcb2
ramotk02 <omartaky.ot@gmail.com>
2024-01-02 20:29:30 +0100

    up

--- code.js
+++ code.js
@@ -1,14 +1,26 @@
-const display = document.getElementById("display");
-function appendToDisplay(input) {
-  display.value += input;
-}
-function deleteFromDisplay() {
-  display.value = "";
-}
-function calculate() {
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    display.value = "ERROR";
-  }
-}
+
+ const display = document.getElementById("display");
+
+    function appendToDisplay(input) {
+    display.value += input;
+    }
+    function calculate() {
+        try {
+            display.value = eval(display.value);
+        } catch (error) {
+            display.value = "ERROR";
+        }
+        }
+    function resetDisplay() {
+    display.value = "";
+    }
+    function deleteFromDisplay() {
+        var currentDisplayValue = display.value;
+    
+        if (currentDisplayValue.length > 0) {
+        currentDisplayValue = currentDisplayValue.slice(0, -1);
+        display.value = currentDisplayValue;
+        }
+    }
+    
+    
