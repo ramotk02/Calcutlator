4df5398823a671ba9c9d82e980588f393bf547a7
ramotk02 <omartaky.ot@gmail.com>
2024-01-03 09:40:01 +0100

    up

--- code.js
+++ code.js
@@ -4,13 +4,14 @@
     function appendToDisplay(input) {
     display.value += input;
     }
-    
+
     function calculate() {
         try {
             display.value = eval(display.value);
         } catch (error) {
             display.value = "ERROR";
         }
+    
         }
     function resetDisplay() {
         display.value = "";
@@ -24,5 +25,7 @@
         display.value = currentDisplayValue;
         }
     }
+    
+
     
     
